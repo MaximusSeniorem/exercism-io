5f2d4cfa8881e4ed8546b41a190670d104460f41
Maxime Lainé <9930974+captfricasse@users.noreply.github.com>
2019-09-13 12:52:53 +0200

    rust: reverse-string, added explainations

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn reverse(input: &str) -> String {
     input.chars().rev().collect() //collect::<String>()
 }
+
+/*
+str.chars()
+returns an iterable on a String characters
+
+iter.rev()
+reverse itrator's direction
+
+iter.collect::<String>()
+Transforms an iterator into a collection, String being a collection of chars
+
+::<>
+"turbo fish" helps the inferene system deduce the type
+mandatory here since we have the return type of the function
+*/
