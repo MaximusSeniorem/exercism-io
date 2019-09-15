038ede51a9cc4855c186406fd50df415b325db20
Maxime Lainé <9930974+captfricasse@users.noreply.github.com>
2019-09-16 00:15:01 +0200

    rust: reverse-string, adding graphemes support from external lib (crate)

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,18 +1,26 @@
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect() //collect::<String>()
+    UnicodeSegmentation::graphemes(input, true).rev().collect()
 }
 
-/*
+/*works fine bu does not handle graphemes
+pub fn reverse(input: &str) -> String {
+    input.chars().rev().collect()
+}
+
 str.chars()
 returns an iterable on a String characters
 
 iter.rev()
 reverse itrator's direction
 
-iter.collect::<String>()
-Transforms an iterator into a collection, String being a collection of chars
+iter.collect()
+Transforms an iterator into a collection
 
-::<>
-"turbo fish" helps the inferene system deduce the type
-mandatory here since we have the return type of the function
+alt:
+::<String>
+"turbofish" helps the inference system deduce the type to collect to
+not mandatory here since we have the return type of the function
 */
