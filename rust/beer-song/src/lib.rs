c5acb6a61d1abcb4409049a37092a32bd95298db
Maxime Lainé <9930974+captfricasse@users.noreply.github.com>
2019-09-18 16:26:02 +0200

    rust bonus: beer-song simple

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,17 @@
 pub fn verse(n: i32) -> String {
-    unimplemented!("emit verse {}", n)
+    match n {
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
+        2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
+        _ => format!("{x} bottles of beer on the wall, {x} bottles of beer.\nTake one down and pass it around, {x1} bottles of beer on the wall.\n", x=n, x1=n-1).to_string()
+    }
 }
 
 pub fn sing(start: i32, end: i32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    let mut verses = vec![];
+    for i in (end..=start).rev() {
+        if i != end { verses.push(format!("{}\n", verse(i))); }
+        else { verses.push(verse(i)); }
+    }
+    verses.concat()
 }
