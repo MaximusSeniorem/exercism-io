6daed09b28d7cdf0061e586e9d555dcffd2748c1
Maxime Lainé <9930974+captfricasse@users.noreply.github.com>
2019-09-25 21:15:12 +0200

    rust bonus: sum-of-multiples finished

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,7 +1,7 @@
+fn is_mult(n: &u32, factors: &[u32]) -> bool {
+    factors.iter().any(|x| *x != 0 && n % x == 0)
+}
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    (1..limit).fold(0, |acc, x| if is_mult(&x, factors) { acc + x } else { acc })
 }
