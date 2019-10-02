7cab05d955da087f35b9ab434033c75f61f07a9a
Maxime Lainé <9930974+captfricasse@users.noreply.github.com>
2019-10-02 16:28:40 +0200

    rust bonus: grains finished - better

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,10 +1,16 @@
 pub fn square(s: u32) -> u64 {
-    if s < 1 || s > 64 { panic!("Square must be between 1 and 64"); }
-    2_u64.pow(s-1)
+    match s {
+        1..=64 => 2_u64.pow(s - 1),
+        _ => panic!("Square must be between 1 and 64")
+    }
 }
 
 pub fn total() -> u64 {
+    /*
     (1..=64)
         .map(|x| square(x))
         .sum()
+    That's a const value: sum(n=0-63){2^n}
+    */
+    0xFFFFFFFFFFFFFFFF
 }
