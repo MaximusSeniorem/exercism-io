362a1606d7605136c0285e5aa95f91f166e5baf2
Maxime Lainé <9930974+captfricasse@users.noreply.github.com>
2019-10-02 16:56:30 +0200

    rust bonus: prime-factors finished

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,6 @@
+use primes::PrimeSet;
+
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+   PrimeSet::new()
+       .prime_factors(n)
 }
