517097d517d87bd5d37d68109a9665c0ad5b7d64
Maxime Lainé <9930974+captfricasse@users.noreply.github.com>
2019-10-02 18:22:54 +0200

    rust bonus: armstrong-numbers started and finished

---
+++ rust/armstrong-numbers/src/lib.rs
@@ -0,0 +1,21 @@
+fn list_digit(n : u32) -> Vec<u32> {
+    if n == 0 { return vec![0]; }
+    let mut x = n;
+
+    std::iter::from_fn(move || {
+        if x == 0 {
+            None
+        } else {
+            let current = x % 10;
+            x /= 10;
+            Some(current)
+        }
+    }).collect::<Vec<u32>>()
+}
+
+pub fn is_armstrong_number(num: u32) -> bool {
+    let list = list_digit(num);
+    let pow = list.len() as u32;
+
+    num == list.iter().map(|x| x.pow(pow)).sum()
+}
