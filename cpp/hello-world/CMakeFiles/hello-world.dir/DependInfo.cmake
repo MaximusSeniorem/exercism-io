3c021938cf33754f9d08cbf470bb4f69b5eb4bc5
Maximus Seniorem <9930974+MaximusSeniorem@users.noreply.github.com>
2022-03-12 17:16:58 +0100

    add cpp track and hello world

---
+++ cpp/hello-world/CMakeFiles/hello-world.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/repos/exercism-io/cpp/hello-world/hello_world.cpp" "E:/repos/exercism-io/cpp/hello-world/CMakeFiles/hello-world.dir/hello_world.cpp.obj"
+  "E:/repos/exercism-io/cpp/hello-world/hello_world_test.cpp" "E:/repos/exercism-io/cpp/hello-world/CMakeFiles/hello-world.dir/hello_world_test.cpp.obj"
+  "E:/repos/exercism-io/cpp/hello-world/test/tests-main.cpp" "E:/repos/exercism-io/cpp/hello-world/CMakeFiles/hello-world.dir/test/tests-main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
